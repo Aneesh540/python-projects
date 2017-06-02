300bc2213be44bf4642a446d6dcbcf2fe5e68c6a
Aneesh540 <aneeshjain203@gmail.com>
2017-06-03 00:57:11 +0530

    commit-3: >>> SEE IT<<< Vector(class) representation with vector addn scalar multiplication and modulus 				NOTE:VECTOR MULTIPLY IS NOT WORKING example v1=Vector(3,4) v1*3=Vector(9,12)

--- cbse.py
+++ cbse.py
@@ -1,16 +1,53 @@
-""" a static method in pyhton is a method that does not obey
-	the usual convection in which self, an instance of the class 
-	is first argument to the method
+"""class for vector *, +, /,- and many others 
+    hypot function gives -/x**2 + y**2 i.e. hypotaneous    
+"""
+from math import hypot,sqrt
 
-	it is just like any other method of class but canbe called without
-	creating the instance of the class
-	@staticmethod is convection for using it."""
+class Vector:
+    """docstring for Vector class 
+        NOTE: len(v) length of vector gives different result 
+        i.e. x+y coz here we use different length method
+        
+    """
+    def __init__(self, x=0, y=0):
+        self.x=x
+        self.y=y
 
-class Test:
-	@staticmethod
-	def display(x):
-		return x*x
+    def __repr__(self):
+        """ instead of <'class' object at 0x7.....>
+            it returns this for better user eperience
+        """
+        return 'Vector(%r,%r)' %(self.x,self.y)
 
-bar=Test()
-print(bar.display(5))
+    def __abs__(self):
+        return hypot(self.x,self.y)
 
+    def __add__(self,others):
+        return Vector(self.x + others.x,self.y+others.y)
+
+    def __mul__(self,scalar):
+        return Vector(self.x*scalar,self.y*scalar)
+
+    def __len__(self):
+        return (self.x+self.y)
+
+    def __rmul__(self,vector2):
+        """vector Multiplication ka magic method
+            BUT NOT WORKING """
+        return self.x * vector2.x + self.y * vector2.y    
+
+if __name__ == '__main__':
+        
+    x,y=map(int,input('enter 1st vector v1= ').split())
+
+    v1=Vector(x,y)
+    print('\n __repr__',v1) 
+    print('__abs__(v1)',abs(v1))
+    print('__len__(v1)',len(v1))
+
+    z,w = map(int,input('\n now enter 2st vector v2= ').split())
+    v2=Vector(z,w)
+    print('v1 +v2',v1+v2)
+    #print(v1*v2)
+    print('v2 *3 =',v2*3)
+    print(abs(v2))
