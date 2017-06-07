41ff3628498d2b577095cf28efa7fbcfa34ea04e
Aneesh540 <aneeshjain203@gmail.com>
2017-06-08 02:24:33 +0530

    commit-5:copy() and deepcopy() version1.0,by Bus class example

--- classes_master.py
+++ classes_master.py
@@ -1,26 +1,42 @@
-"""How python works and deep and shallow copy"""
+"""use of copy() and deepcopy() for handling previous commit anomoly"""
+import copy
 
-foo=[1,[2,3,4],(5,6,7)]
-bar=foo
+class Bus:
+    """School Bus which pick and drops passenger in route"""
+    def __init__(self,passenger=[]):
+        self.passenger=list(passenger)
 
-foo.append(100)
-print(foo)
-print(bar)
-# foo and bar are refrence to [1,[2,3,4],(5,6,7)]
-#so both will change
+    def pick(self,name):
+        self.passenger.append(name)
 
-chicken = [12,[22,45,78],(123,432,1993)]
-rooster=list(chicken)
+    def drop(self,name):
+        self.passenger.remove(name)
+    
+    def __str__(self):
+         return '{}'.format(self.passenger)   
 
-chicken.append('fooo&baar')
-rooster.append('aneeshjain')
-rooster[1] += [34,59]
-rooster[2] += (234,236)
+def main():
+    bus1=Bus(['angular js','React js','Node js','Grunt js'])
+    print('bus1:',bus1,'\n')
+    bus2=copy.copy(bus1)
+    bus3=copy.deepcopy(bus1)
 
-print(chicken)
-print(rooster)
+    dopel=bus1
 
-print('+'*50,'\n','+'*50)
+    print('bus1-id:%s\nbus2-id:%s\nbus3-id:%s\nbus4-id:%s' %(id(bus1),id(bus2),id(bus3),id(dopel)))
+    print('\nnow removing angular js from bus 1\n')
+    bus1.drop('angular js')
+    print('bus1:',bus1)
+    print('bus2:',bus2)
+    print('bus3:',bus3)
+    print('dopel:',dopel)
 
+    print('\npicking aurelia js in bus3\n')
+    bus3.pick('aurelia js')
+    print('bus1:',bus1)
+    print('bus2:',bus2)
+    print('bus3:',bus3)
+    print('dopel:',dopel)
 
-
+if __name__ == '__main__':
+        main()    
